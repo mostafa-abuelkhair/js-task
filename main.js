59243343298754c563c5d7b2dbd8b9611eb57308
mostafa ahmed <mostafa.ahmed2509@gmail.com>
2023-05-27 19:25:45 +0300

    finished

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+
+function add(){
+    let input1=document.getElementById("input1").value;
+    let input2=document.getElementById("input2").value;
+    sum= Number(input1)+Number(input2);
+    alert(sum);
+}
